ed41c488abb4dbbc7a6a75c3a10b5568a324efc5
Afolabi Olajide Samuel <samolajide20@gmail.com>
2022-08-01 20:32:58 +0100

    added wallet connect feature

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
 import './App.css';
 import About from './components/about/About';
 import Hero from './components/hero/Hero';
@@ -6,9 +9,56 @@ import Mint from './components/mint/Mint';
 import Nav from './components/nav/Nav';
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    // Setup Listeners on Metamast event changes
+    if (window.ethereum) {
+      // Add Listener when accounts switch
+      window.ethereum.on("accountsChanged", (accounts) => {
+        toast.info('Your account has been changed', {
+          position: "top-center",
+          autoClose: 9000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+        
+        setAccounts(accounts);
+      });
+      // Do something when chain changes
+      window.ethereum.on("chainChanged", (chainId) => {
+        toast.info('Login again to switch to Ethereum mainnet', {
+          position: "top-center",
+          autoClose: 9000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+        setAccounts([]);
+        setIsConnected(false);
+      });
+    } else {
+      toast.error('Please install Metamask to use this app', {
+        position: "top-center",
+        autoClose: 9000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+    }
+  }, [isConnected]);
+
   return (
     <div className="App">
-      <Nav />
+      <Nav accounts={accounts} setAccounts={setAccounts} setIsConnected={setIsConnected} />
       <Hero />
       <About />
       <Mint />
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,35 +5,121 @@ import twitter from "../../assets/images/twitter.png";
 import discord from "../../assets/images/discord.png";
 import telegram from "../../assets/images/telegram.png";
 
-const Nav = () => {
-    const [navIsActive, setNavIsActive] = useState(false)
+const Nav = ( { accounts, setAccounts, setIsConnected } ) => {
+	const [navIsActive, setNavIsActive] = useState(false);
 
-    const toggleNav = () => {
-        setNavIsActive(!navIsActive);
-    }
+	const toggleNav = () => {
+		setNavIsActive(!navIsActive);
+	};
+
+	const isConnect = Boolean(accounts[0]);
+	console.log(accounts);
+
+	async function loginWithMetaMask() {
+		if (window.ethereum.chainId !== "0x89") {
+			try {
+				await window.ethereum.request({
+					method: "wallet_switchEthereumChain",
+					params: [{ chainId: "0x89" }],
+				});
+			} catch (switchError) {
+				// This error code indicates that the chain has not been added to MetaMask.
+				if (switchError.code === 4902) {
+					try {
+						await window.ethereum.request({
+							method: "wallet_addEthereumChain",
+							params: [
+								{
+									chainId: "0x89",
+									blockExplorerUrls: [
+										"https://polygonscan.com/",
+									],
+									chainName: "Matic Mainnet",
+									iconUrls: "",
+									nativeCurrency: {
+										name: "MATIC",
+										symbol: "MATIC",
+										decimals: 18,
+									},
+									rpcUrls: ["https://polygon-rpc.com/"],
+								},
+							],
+						});
+					} catch (addError) {
+						// handle "add" error
+					}
+				}
+				// handle other "switch" errors
+			}
+			// ------------------------------
+			const accounts = await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
+			setAccounts(accounts);
+			setIsConnected(true);
+			// checkOwner(accounts);
+		} else {
+			const accounts = await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
+
+			setAccounts(accounts);
+			setIsConnected(true);
+			// checkOwner(accounts);
+		}
+		// window.userWalletAddress = accounts[0];
+	}
+
+	const connect = () => {
+		if (!window.ethereum) {
+			alert("MetaMask is not installed!");
+		} else {
+			loginWithMetaMask();
+		}
+	};
 
 	return (
 		<nav className="nav">
 			<div className="nav-logo">LOGO</div>
-			<div className={`nav-items ${navIsActive ? "active" : "" }`}>
+			<div className={`nav-items ${navIsActive ? "active" : ""}`}>
 				<div className="nav-item">
-					<a href="#about" onClick={toggleNav}>About</a>
+					<a href="#about" onClick={toggleNav}>
+						About
+					</a>
 				</div>
 				<div className="nav-item">
-					<a href="#mint" onClick={toggleNav}>Mint</a>
+					<a href="#mint" onClick={toggleNav}>
+						Mint
+					</a>
 				</div>
 				<div className="nav-item">
-					<a href="#community" onClick={toggleNav}>Community</a>
+					<a href="#community" onClick={toggleNav}>
+						Community
+					</a>
 				</div>
 			</div>
 			<div className="nav-cta">
 				<div className="socials">
-					<span className="social"><a href="https://"><img src={discord} alt="" /></a></span>
-					<span className="social"><a href="https://"><img src={twitter} alt="" /></a></span>
-					<span className="social"><a href="https://"><img src={telegram} alt="" /></a></span>
+					<span className="social">
+						<a href="https://">
+							<img src={discord} alt="" />
+						</a>
+					</span>
+					<span className="social">
+						<a href="https://">
+							<img src={twitter} alt="" />
+						</a>
+					</span>
+					<span className="social">
+						<a href="https://">
+							<img src={telegram} alt="" />
+						</a>
+					</span>
 				</div>
-				<span class="material-icons-sharp menu" onClick={toggleNav}>menu</span>
-				<button className="btn">Connect Wallet</button>
+				<span className="material-icons-sharp menu" onClick={toggleNav}>
+					menu
+				</span>
+				{ isConnect ? <button className="btn">{accounts[0].split("", 4)}...{accounts[0].split("", 6)}</button> : <button onClick={connect} className="btn">Connect Wallet</button> }
 			</div>
 		</nav>
 	);
